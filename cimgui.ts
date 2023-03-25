9881257db338d84bc556ce4bf59a8b1dfd3dbabb
Andre Weissflog <floooh@gmail.com>
2023-03-25 19:05:01 +0100

    update syntax

--- cimgui.ts
+++ cimgui.ts
@@ -5,37 +5,35 @@ export const project: fibs.ProjectDesc = {
         cimgui: {
             url: 'https://github.com/cimgui/cimgui',
             ref: '1.89.3',
-            projects: [
-                {
-                    targets: {
-                        cimgui: {
-                            type: 'lib',
-                            sources: [
-                                'cimgui.cpp',
-                                'cimgui.h',
-                                'imgui/imgui_demo.cpp',
-                                'imgui/imgui_draw.cpp',
-                                'imgui/imgui_tables.cpp',
-                                'imgui/imgui_widgets.cpp',
-                                'imgui/imgui.cpp',
-                                'imgui/imgui.h',
-                            ],
-                            includeDirectories: {
-                                public: [ '.' ]
-                            },
-                            compileOptions: {
-                                private: (context) => {
-                                    if (context.compiler === 'gcc') {
-                                        return ['-Wno-stringop-overflow'];
-                                    } else {
-                                        return [];
-                                    }
+            project: {
+                targets: {
+                    cimgui: {
+                        type: 'lib',
+                        sources: [
+                            'cimgui.cpp',
+                            'cimgui.h',
+                            'imgui/imgui_demo.cpp',
+                            'imgui/imgui_draw.cpp',
+                            'imgui/imgui_tables.cpp',
+                            'imgui/imgui_widgets.cpp',
+                            'imgui/imgui.cpp',
+                            'imgui/imgui.h',
+                        ],
+                        includeDirectories: {
+                            public: [ '.' ]
+                        },
+                        compileOptions: {
+                            private: (context) => {
+                                if (context.compiler === 'gcc') {
+                                    return ['-Wno-stringop-overflow'];
+                                } else {
+                                    return [];
                                 }
                             }
                         }
                     }
                 }
-            ],
+            }
         }
     }
 }
