9564333216eeb56b7dc88f1e2f72cb41fbbb821e
Andre Weissflog <floooh@gmail.com>
2023-03-25 19:39:31 +0100

    add stdoptions.ts and shell.html

---
+++ stdoptions.ts
@@ -0,0 +1,28 @@
+// a collection of 'opinionated' standard compile options for inclusion in other projects
+import { fibs } from './deps.ts';
+
+export const project: fibs.ProjectDesc = {
+    // language standards
+    variables: {
+        CMAKE_C_STANDARD: '11',
+        CMAKE_CXX_STANDARD: '14',
+    },
+
+    // increase warning levels
+    compileOptions: (context) => {
+        if (context.compiler === 'msvc') {
+            return [ '/W4' ];
+        } else {
+            return [ '-Wall', '-Wextra' ];
+        }
+    },
+
+    // on Emscripten, use our custom shell.html for all exe targets
+    linkOptions: (context) => {
+        if (context.config.platform === 'emscripten') {
+            return [ '--shell-file', '@root/src/shell.html' ];
+        } else {
+            return [];
+        }
+    },
+}
