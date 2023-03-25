a1806ef4bf8a11e3761d41cd3993bc3f13d25c3d
Andre Weissflog <floooh@gmail.com>
2023-03-25 19:14:33 +0100

    add sokol.ts

---
+++ sokol.ts
@@ -0,0 +1,76 @@
+import { fibs } from './deps.ts';
+
+export const project: fibs.ProjectDesc = {
+    imports: {
+        sokol: {
+            url: 'https://github.com/floooh/sokol',
+            project: {
+                targets: {
+                    'sokol-interface': {
+                        type: 'interface',
+                        includeDirectories: {
+                            interface: [ '.', './util' ]
+                        },
+                        compileDefinitions: {
+                            interface: (context) => {
+                                switch (context.config.platform) {
+                                    case 'windows':
+                                        return [ 'SOKOL_D3D11' ];
+                                    case 'macos':
+                                    case 'ios':
+                                        return [ 'SOKOL_METAL' ];
+                                    case 'emscripten':
+                                    case 'android':
+                                        return [ 'SOKOL_GLES3' ];
+                                    default:
+                                        return [ 'SOKOL_GLCORE33' ];
+                                }
+                            }
+                        },
+                        compileOptions: {
+                            interface: (context) => {
+                                switch (context.config.platform) {
+                                    case 'macos': case 'ios':
+                                        return ['--language', 'objective-c'];
+                                    case 'linux':
+                                        return ['-pthread'];
+                                    default:
+                                        return [];
+                                }
+                            }
+                        },
+                        linkOptions: {
+                            interface: (context) => {
+                                switch (context.config.platform) {
+                                    case 'emscripten':
+                                        return [ '-sUSE_WEBGL2=1', "-sMALLOC='emmalloc'" ];
+                                    case 'linux':
+                                        return [ '-pthread', '-lpthread' ];
+                                    default:
+                                        return [];
+                                }
+                            }
+                        },
+                        libs: (context) => {
+                            switch (context.config.platform) {
+                                case 'macos':
+                                    return [
+                                        '-framework Foundation',
+                                        '-framework Cocoa',
+                                        '-framework QuartzCore',
+                                        '-framework Metal',
+                                        '-framework MetalKit',
+                                        '-framework AudioToolbox',
+                                    ];
+                                case 'linux':
+                                    return [ 'X11', 'Xi', 'Xcursor', 'GL', 'asound' ];
+                                default:
+                                    return [];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
