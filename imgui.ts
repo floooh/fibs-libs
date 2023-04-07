d0b0c2c7706de6e6f29536cf7c8668f28bc83106
Andre Weissflog <floooh@gmail.com>
2023-04-07 20:42:03 +0200

    add imgui.ts

---
+++ imgui.ts
@@ -0,0 +1,37 @@
+import { fibs } from './deps.ts';
+
+export const project: fibs.ProjectDesc = {
+    imports: {
+        imgui: {
+            url: 'https://github.com/ocornut/imgui',
+            ref: '1.89.4',
+            project: {
+                targets: {
+                    imgui: {
+                        type: 'lib',
+                        sources: [
+                            'imgui_demo.cpp',
+                            'imgui_draw.cpp',
+                            'imgui_tables.cpp',
+                            'imgui_widgets.cpp',
+                            'imgui.cpp',
+                            'imgui.h',
+                        ],
+                        includeDirectories: {
+                            public: [ '.' ]
+                        },
+                        compileOptions: {
+                            private: (context) => {
+                                if (context.compiler === 'gcc') {
+                                    return ['-Wno-stringop-overflow'];
+                                } else {
+                                    return [];
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
