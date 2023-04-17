33637cdcd75db65fe34aade5d64a59ad41b7de4d
Andre Weissflog <floooh@gmail.com>
2023-04-17 15:21:57 +0200

    add nuklear.ts

---
+++ nuklear.ts
@@ -0,0 +1,38 @@
+import { fibs } from './deps.ts';
+
+export const project: fibs.ProjectDesc = {
+    imports: [
+        {
+            name: 'nuklear',
+            url: 'https://github.com/Immediate-Mode-UI/Nuklear',
+            project: {
+                targets: [
+                    {
+                        name: 'nuklear',
+                        type: 'interface',
+                        includeDirectories: {
+                            interface: () => [ '.' ],
+                        },
+                        compileOptions: {
+                            interface: (ctx) => {
+                                if (ctx.compiler === 'msvc') {
+                                    return [ '/wd4996']
+                                } else {
+                                    return [
+                                        '-Wno-unknown-warning-option',
+                                        '-Wno-unused-parameter',
+                                        '-Wno-sign-conversion',
+                                        '-Wno-null-pointer-subtraction',
+                                        '-Wno-unused-but-set-variable',
+                                        '-Wno-maybe-uninitialized',
+                                        '-Wno-unused-parameter',
+                                    ]
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ]
+}
