88580e43abf4733900f036a5d6ce8439a102dcbf
Andre Weissflog <floooh@gmail.com>
2025-12-20 18:54:13 +0100

    sokol.ts: fix option typo

--- sokol.ts
+++ sokol.ts
@@ -25,7 +25,7 @@ export function build(b: fibs.Builder) {
         t.addCompileDefinitions({ defs: { [`SOKOL_${backend.toUpperCase()}`]: '1' }, scope: 'interface' });
         if (b.isMacOS() || b.isIOS()) {
             t.addCompileOptions({ opts: ['--language objective-c++'], language: 'cxx' });
-            t.addCompileOptions({ opts: ['--language objecttive-c'], language: 'c' });
+            t.addCompileOptions({ opts: ['--language objective-c'], language: 'c' });
             t.addLibraries(['-framework Foundation', '-framework AudioToolbox']);
             if (b.isMacOS()) {
                 t.addLibraries(['-framework Cocoa', '-framework Quartzcore']);
